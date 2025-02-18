aa0bdb6c073d10e4358897e390a8a6c9ae19f932
wu, zhe <1127783699@qq.com>
2025-02-18 22:24:30 +0800

    ci config

---
+++ src/feature/PostCard.js
@@ -0,0 +1,9 @@
+import { Card } from 'antd';
+function PostCard() {
+  return (
+    <div style={{ marginTop: '16px' }}>
+      <Card title="Card Title"></Card>
+    </div>
+  );
+}
+export default PostCard;
